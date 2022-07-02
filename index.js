9b5ce108618c17d45315d8a97726434ec605ae33
Sudharsan <104492402+Sudharsan7777@users.noreply.github.com>
2022-07-02 20:50:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+function increment()
+{
+    var number = Number(document.getElementById('number').innerHTML)
+
+    document.getElementById('number').innerHTML = number+1
+}
+
+function decrement()
+{
+    var number = Number(document.getElementById('number').innerHTML)
+
+    document.getElementById('number').innerHTML = number-1
+}
+
+function neutral()
+{
+    document.getElementById('number').innerHTML = 0
+}
+
+function mby2()
+{
+    var number = Number(document.getElementById('number').innerHTML)
+
+    document.getElementById('number').innerHTML = number+2
+}
+
+function mby3()
+{
+    var number = Number(document.getElementById('number').innerHTML)
+
+    document.getElementById('number').innerHTML = number+3
+}
+
+function mby4()
+{
+    var number = Number(document.getElementById('number').innerHTML)
+
+    document.getElementById('number').innerHTML = number+4
+}
